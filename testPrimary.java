21c87405af25ada406252e2f1e5f5d85b36ecc84
Chamara Shyamantha <Chamara.848@gmail.com>
2022-07-16 01:40:31 +0530

    Primary Java Class

---
+++ testPrimary.java
@@ -0,0 +1,29 @@
+package four.task;
+
+import static four.task.KUnitTestClass.*;
+
+public class testPrimary {
+
+    void checkConstructorAndAccess() {
+        Primary primary = new Primary(4, 5);
+
+        checkEqualDouble(primary.getX(), 4);
+        checkNotEqualDouble(primary.getX(), 4);
+        checkEqualDouble(primary.getY(), 5);
+        checkNotEqualDouble(primary.getY(), 5);
+
+    }
+
+    void checkPowerX() {
+        Primary primary = new Primary(4, 5);
+        primary.powerX();
+        checkEqualDouble(primary.getX(), 16);
+    }
+
+    public static void main(String[] args) {
+        testPrimary testPrimary = new testPrimary();
+        testPrimary.checkConstructorAndAccess();
+        testPrimary.checkPowerX();
+        report();
+    }
+}
