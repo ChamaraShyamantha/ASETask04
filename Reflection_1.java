977215f6238470526a1d3f6a4b52536dfa6c98ff
Chamara Shyamantha <Chamara.848@gmail.com>
2022-07-16 01:36:34 +0530

    Reflection 01 - Print a simple output

---
+++ Reflection_1.java
@@ -0,0 +1,8 @@
+package four.task;
+
+public class Reflection_1 {
+    public static void main(String[] args) {
+        Primary primary = new Primary();
+        System.out.println(primary);
+    }
+}
