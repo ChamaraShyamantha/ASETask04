f23e4dcc67683382d3828fa0de6030d40dc148af
Chamara Shyamantha <Chamara.848@gmail.com>
2022-07-16 01:38:06 +0530

    Reflection 06 - Checking Accessibility

---
+++ Reflection_6.java
@@ -0,0 +1,22 @@
+package four.task;
+
+import java.lang.reflect.Field;
+
+public class Reflection_6 {
+    public static void main(String[] args) throws IllegalAccessException {
+
+        Primary primary = new Primary();
+
+        Field[] declaredFields = primary.getClass().getDeclaredFields();
+        System.out.println("field count : " + declaredFields.length);
+        System.out.println();
+
+        for (Field field: declaredFields) {
+            System.out.println("Field Name : " + field.getName());
+            System.out.println("Type : " + field.getType());
+            System.out.println("Value : " + field.isAccessible());
+            System.out.println();
+        }
+
+    }
+}
